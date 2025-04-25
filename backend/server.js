598bce6dd41586c503abd29f3a8de4f43a404670
salmanshaikssk007 <salmanshaikssk007@gmail.com>
2025-04-25 17:28:03 -0500

    built: created containers for the project

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://frontend:80",
     // credentials: true,
   },
 });
